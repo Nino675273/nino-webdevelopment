0ec44c2800cfe65e544bc5116ae8f4647bfcb73f
Nino675273 <70753420+Nino675273@users.noreply.github.com>
2022-01-30 17:17:36 +0100

    test

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,22 @@
 <?php
 include __DIR__ . '/../header.php';
+?>
 
-echo "<h1>About page!</h1>";
+<div class="bg-light text-center p-5">
+<h1 class="display-3">About SecondLap</h1>
+</div>
+    <section>
+        <div class="container">
+            <h2>history</h2>
+            <p>SecondLabs bestond sinds 2020</p>
+            <h3>Wat bieden we aan de klanten?</h3>
+            <p>We geven de mogelijkheid om de klanten hun laptops te mogen verkopen EN kopen van andere mede klanten.</p>
+            </div>
+    </section>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+<?php
 include __DIR__ . '/../footer.php';
+?>
