022d99e6af0ef064571c59a62e4c318816afc037
Nino675273 <70753420+Nino675273@users.noreply.github.com>
2022-04-08 17:17:58 +0200

    Update articlerepository.php

--- app/repositories/articlerepository.php
+++ app/repositories/articlerepository.php
@@ -11,7 +11,7 @@ class ArticleRepository extends Repository
             
             $stmt->execute();
             
-            $stmt->setFetchMode(PDO::FETCH_CLASS, 'Product');
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'Article');
             $products = $stmt->fetchAll();
 
             return $products;
