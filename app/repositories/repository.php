26dd995b7b35af66b8cc606ce6265ba38456ae62
Nino675273 <70753420+Nino675273@users.noreply.github.com>
2022-04-08 16:08:29 +0200

    Update repository.php

--- app/repositories/repository.php
+++ app/repositories/repository.php
@@ -1,20 +1,19 @@
 <?php
 
-class Repository {
+class Repository{
 
     protected $connection;
 
-    function __construct() {
-
+    function __construct()
+    {
         require __DIR__ . '/../dbconfig.php';
 
         try {
-            $this->connection = new PDO("$type:host=$servername;dbname=$database", $username, $password);
-                
+            $this->connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
             // set the PDO error mode to exception
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
-          }
-    }       
+        }
+    }
 }
